87608d15a66d32f82b857819d9e9342cd8b52966
Link1515 <h26281660@gmail.com>
2024-05-30 16:10:13 +0800

    add warning when checking port between 0 ~ 1023

--- checkPortAvailable.sh
+++ checkPortAvailable.sh
@@ -3,21 +3,19 @@
 RESET_STYLE="\033[0m"
 RED_COLOR="\033[0;31m"
 GREEN_COLOR="\033[0;32m"
+YELLOW_COLOR="\033[0;33m"
 RED_BACKGROUND="\033[0;41m"
 GREEN_BACKGROUND="\033[0;42m"
+YELLOW_BACKGROUND="\033[0;43m"
 
-echo ""
-echo "##############################"
-echo "     check port available     "
-echo "##############################"
 echo ""
 
 inputPort=$1
 
-if [ -z "$inputPort" ]; then
+if [[ -z "$inputPort" ]]; then
   echo -e "${RED_BACKGROUND} ERROR ${RESET_STYLE}"
   echo -e "${RED_COLOR}Please pass the port you want to check.${RESET_STYLE}"
-  echo -e "${RED_COLOR}Usage: ./checkPortAvailable [port]${RESET_STYLE}"
+  echo -e "${RED_COLOR}Usage: ./checkPortAvailable.sh [port]${RESET_STYLE}"
   echo ""
   exit 1
 fi
@@ -36,17 +34,23 @@ if (($inputPort > 65535 || $inputPort < 0)); then
   exit 1
 fi
 
+if (($inputPort < 1024 && $inputPort >= 0)); then
+  echo -e "${YELLOW_BACKGROUND} WARNING ${RESET_STYLE}"
+  echo -e "${YELLOW_COLOR}Port 0~1023 is usually reserved for system use.${RESET_STYLE}"
+  echo ""
+fi
+
 ports=$(ss -tulpn | grep LISTEN | awk '{ print $5 }' | awk -F: '{ print $NF }' | sort -n | uniq)
 
 for port in $ports; do
   if [ "$inputPort" = "$port" ]; then
     echo -e "${RED_BACKGROUND} ERROR ${RESET_STYLE}"
-    echo -e "${RED_COLOR}This port $inputPort is currently in use!${RESET_STYLE}"
+    echo -e "${RED_COLOR}Port $inputPort is currently in use!${RESET_STYLE}"
     echo ""
     exit 1
   fi
 done
 
 echo -e "${GREEN_BACKGROUND} OK! ${RESET_STYLE}"
-echo -e "${GREEN_COLOR}This port $inputPort is available!${RESET_STYLE}"
+echo -e "${GREEN_COLOR}Port $inputPort is available!${RESET_STYLE}"
 echo ""
